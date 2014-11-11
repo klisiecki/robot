67aefd8a229bdaf9b9a3237950542185d1346685
Karol Lisiecki <lisiecki.karol@gmail.com>
2014-11-11 19:44:26 +0100

    created Config & MyJavaCameraView

---
+++ RobotProject/src/pl/poznan/put/ioiorobot/camera/MyJavaCameraView.java
@@ -0,0 +1,20 @@
+package pl.poznan.put.ioiorobot.camera;
+
+import org.opencv.android.JavaCameraView;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.AttributeSet;
+
+public class MyJavaCameraView extends JavaCameraView {
+	
+	public Camera getCamera() {
+		return mCamera;
+	}
+
+	public MyJavaCameraView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ RobotProject/src/pl/poznan/put/ioiorobot/utils/Config.java
@@ -0,0 +1,5 @@
+package pl.poznan.put.ioiorobot.utils;
+
+public class Config {
+	public static final double thresholdFactor = 0.5;
+}
